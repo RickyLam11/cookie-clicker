c00fc78fe81544063cefcc621bc6920f0ecf4ca5
Ricky From Hong Kong <lamricky11@hotmail.com>
2022-07-21 16:26:28 +0200

    refactor: group all js into one file

---
+++ js/loadScript.js
@@ -0,0 +1,15 @@
+function loadScript(url)
+{    
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.async = true;
+    head.appendChild(script);
+}
+
+loadScript('./js/support.js')
+loadScript('./js/cursorClass.js')
+loadScript('./js/gui.js')
+loadScript('./js/save.js')
+loadScript('./js/main.js')
